d470eb5d91cfe0121d49d76470fa469dc9816073
joseph-salmingo <joseph.salmingo@gmail.com>
2020-12-11 12:08:38 +0900

    various renames, no global, joltdiff change

--- day10/main.py
+++ day10/main.py
@@ -1,3 +1,5 @@
+import functools
+import operator
 
 
 def get_input(file_path: str) -> list:
@@ -5,63 +7,72 @@ def get_input(file_path: str) -> list:
         return [int(text) for text in f.readlines() if text is not None]
 
 
-def volt_multiplier(adapter_set: set) -> dict:
+def get_jolt_diffs(node_dict: dict) -> dict:
 
-    diff_memo = {1: 0, 2: 0, 3: 0}
+    volt_checker = [1, 3]
+    res = {volt: 0 for volt in volt_checker}
 
-    current_volt = 0
-    while len(adapter_set) != 0:
-        for volt_diff in range(1, 4):
-            new_volt = current_volt + volt_diff
-            if new_volt in adapter_set:
-                diff_memo[volt_diff] += 1
-                adapter_set.remove(new_volt)
-                current_volt = new_volt
+    for node in node_dict.keys():
+        for volt in volt_checker:
+            if node + volt in node_dict[node]:
+                res[volt] += 1
                 break
-    return diff_memo
+
+    return res
 
 
-def node_builder(adapter_set: set) -> dict:
+def build_node_list(adapter_set: set) -> dict:
 
-    adapter_list = list(adapter_set)
-    adapter_list.sort()
-    node_list = dict()
-    for adapter in adapter_list:
-        destinations = [f+adapter for f in range(1, 4) if f+adapter in adapter_set]
+    jolt_diff_range = range(1, 4)
+
+    def build_destination_list(adapter: int, comp_set: set) -> list:
+
+        test_set = set([adapter + jolt_diff for jolt_diff in jolt_diff_range])
+        destinations = list(test_set.intersection(comp_set))
         destinations.sort()
-        node_list[adapter] = destinations
+        return destinations
+
+    node_list = {adapter: build_destination_list(adapter, adapter_set) for adapter in adapter_set}
 
     return node_list
 
 
-def path_counter_graph(node_dict: dict, start_node: int) -> int:
-
-    global history
+def count_all_paths(node_dict: dict, start_node: int, history: dict) -> (int, dict):
 
     my_sum = 0
     for node in node_dict[start_node]:
         if node in history.keys():
             my_sum += history[node]
         else:
-            res = path_counter_graph(node_dict, node)
+            res, history = count_all_paths(node_dict, node, history)
             my_sum += res
     if len(node_dict[start_node]) > 1:
         history[start_node] = my_sum
-    return my_sum
+    return my_sum, history
 
 
 if __name__ == '__main__':
 
+    # GET INPUT
     FILE_PATH = 'input.txt'
     num_list = get_input(FILE_PATH)
     num_set = set(num_list)
-    num_set.add(0)
 
     # BUILT IN
+    num_set.add(0)
     num_set.add(max(num_set)+3)
     last_volt = max(num_set)+3
 
-    my_node_dict = node_builder(num_set)
+    # BUILD TABLE
+    my_node_dict = build_node_list(num_set)
+
+    # FIND VOLT DIFFS
+    ret = get_jolt_diffs(my_node_dict)
+    prod = functools.reduce(operator.mul, ret.values())
+    print(f'The product of the 1-jolt differences and 3-jolt differences is {prod}.')
+
+    # FIND ALL BRANCHES
     destination = max(my_node_dict.keys())
-    history = {destination: 1}
-    print(path_counter_graph(my_node_dict, 0))
+    my_history = {destination: 1}
+    all_branches, _ = count_all_paths(my_node_dict, 0, my_history)
+    print(f'There are {all_branches} branches.')
